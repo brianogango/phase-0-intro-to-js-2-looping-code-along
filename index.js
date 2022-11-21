08fc779371867cf80a46f04f60c310de38dc3993
brian <brian.ogango@student.moringaschool.com>
2022-11-21 08:06:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+names = ["Guadalupe","Ollie","Aki"];
+
+function writeCards(names) {
+  let messages = [];
+  names.forEach((name, index) => {
+    messages.push("Thank you, " + name +", for the wonderful surprise gift!");
+  });
+  return messages;
+}
+function countDown(number){
+  while (number >= 0){
+    console.log(number);
+    number--;
+ }
+} 
+
+
+
+
+
+
+
